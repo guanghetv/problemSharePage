078f072eb3d57a57ca53a5540e5a169e6a116cd1
赵岩 <zhaoyan@kongming-inc.com>
2016-06-15 14:23:18 +0800

    feat: 第一版完成

--- js/index.js
+++ js/index.js
@@ -2,17 +2,74 @@
  * Created by zhaoyan on 16/6/13.
  */
 +function() {
-  var formula1 = document.querySelector('#formula1');
+  //var formula1 = document.querySelector('#formula1');
+  var shareBtn = document.querySelector('#share');
+  var shareMask = document.querySelector('#mask-share');
+  var downloadBtn = document.querySelector('.btn-download');
 
   if(Hls.isSupported()) {
     var video = document.getElementById('video');
     var hls = new Hls();
-    hls.loadSource('http://pchls.media.yangcong345.com/pcM_571bc8fc9fcb86114c61cfe5.m3u8');
+    hls.loadSource('http://pchls.media.yangcong345.com/pcM_571b87939fcb86114c61ce95.m3u8');
     hls.attachMedia(video);
     hls.on(Hls.Events.MANIFEST_PARSED,function() {
-      video.play();
+      //video.play();
     });
   }
-  katex.render("x=-\\frac{-2m}{2m}=1", formula1);
+
+  //katex.render("x=-\\frac{-2m}{2m}=1", formula1);
+
+  shareBtn.addEventListener('click', function() {
+    shareMask.className = '';
+  });
+
+  shareMask.addEventListener('click', function() {
+    shareMask.className = 'hidden';
+  });
+
+  downloadBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    downloadApp();
+  });
+
+  function downloadApp(){
+    if(bowser.ios) {
+      window.location = 'http://a.app.qq.com/o/simple.jsp?pkgname=com.yangcong345.android.phone';
+    }
+    if(bowser.android) {
+      downloadAndroidApp();
+    }
+  };
+
+  function downloadAndroidApp() {
+    if(/micromessenger/.test(navigator.userAgent.toLowerCase())) {
+      window.location = 'http://a.app.qq.com/o/simple.jsp?pkgname=com.yangcong345.android.phone';
+    } else {
+      var q = localStorage.q || '';
+      // for android, judge the resource is available or not, if not download default apk;
+      nanoajax.ajax({
+          url: 'http://yc-mobile-content.qiniudn.com/YCMath_Android_V2.5.1_' + q + '.apk',
+          method: 'HEAD'
+        },
+        function (code, responseText, request) {
+          switch(code){
+            case 403:
+            case 400:
+            case 500:
+            case 404:
+              window.location = 'http://yc-mobile-content.qiniudn.com/YCMath_Android_V2.5.1_ychome.apk';
+              break;
+            case 204:
+            case 200:
+              window.location = 'http://yc-mobile-content.qiniudn.com/YCMath_Android_V2.5.1_' + q + '.apk';
+              break;
+            default:
+              window.location = 'http://yc-mobile-content.qiniudn.com/YCMath_Android_V2.5.1_ychome.apk';
+              break;
+          }
+        }
+      );
+    }
+  }
 
 }()
--- js/lib/viewport.js
+++ js/lib/viewport.js
@@ -1,26 +1,30 @@
 ;(function(win) {
-  var doc = win.document;
-  var html = doc.documentElement;
-  var benchmark = 720;  //页面基准宽度（一般为设计稿宽度）
-  var timer;
-
-  function setFontSize() {
-    var htmlWidth = html.getBoundingClientRect().width;
-    var ratio = htmlWidth / benchmark;
-    if(htmlWidth < benchmark) {
-        win.rem = ratio * 1.5;
-    } else {
-        win.rem = 1.5;
-    }
-    win.responseRatio = ratio;
-    html.style.fontSize = win.rem + "rem";
-  }
-
-  win.addEventListener('resize', function() {
-    clearTimeout(timer);
-    timer = setTimeout(setFontSize, 300);
-  }, false);
-
-  setFontSize();
+  //var doc = win.document;
+  //var html = doc.documentElement;
+  //var benchmark = 720;  //页面基准宽度（一般为设计稿宽度）
+  //var timer;
+  //
+  //function setFontSize() {
+  //  var htmlWidth = html.getBoundingClientRect().width;
+  //  console.log('htmlWidth', htmlWidth);
+  //  var ratio = htmlWidth / benchmark;
+  //  if(htmlWidth < benchmark) {
+  //      win.rem = ratio * 1.5;
+  //  } else {
+  //      win.rem = 1.5;
+  //  }
+  //  win.responseRatio = ratio;
+  //  html.style.fontSize = win.rem + "rem";
+  //}
+  //
+  //win.addEventListener('resize', function() {
+  //  clearTimeout(timer);
+  //  timer = setTimeout(setFontSize, 300);
+  //}, false);
+  //
+  //setFontSize();
+  var deviceWidth = document.documentElement.clientWidth;
+  if(deviceWidth > 720) deviceWidth = 720;
+  document.documentElement.style.fontSize = deviceWidth / 7.2 + 'px';
 
 })(window);
