4811f709139d80fba436c228e75fdfe1e7518ddf
xiao-hai <251841702@qq.com>
2016-06-28 16:26:21 +0800

    fix: 微信分享测试

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.task('uglify', ['sass'], function() {
     .pipe(replace(/"\.\.\/img\/answer\.png"/ig, '"@@answer"'))
     .pipe(replace(/"\.\.\/img\/poster\.bmp"/ig, '"@@poster"'))
     .pipe(replace('../img/', ''))
-    .pipe(gulpIf('*.js', uglify().on('error', util.log)))
+    //.pipe(gulpIf('*.js', uglify().on('error', util.log)))
     .pipe(gulpIf('*.css', replace('../img/', '')))
     .pipe(gulpIf('*.css', minifyCss({keepSpecialComments: 0})))
     //.pipe(gulpIf('*.html', minifyHtml({collapseWhitespace: true, removeComments: true, minifyJS: true, minifyCSS: true})))
